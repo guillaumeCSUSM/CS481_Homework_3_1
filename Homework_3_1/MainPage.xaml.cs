e2c8eb254a1e27a34c0d4f2d67cf412279a98ef2
GuillaumeB <>
2019-02-12 16:51:43 -0800

    first commit

---
+++ Homework_3_1/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Homework_3_1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            await Navigation.PushAsync(new SecondPage(userInput.Text));
+        }
+    }
+}
---
+++ Homework_3_1/SecondPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Homework_3_1
+{
+    public partial class SecondPage : ContentPage
+    {
+        public SecondPage()
+        {
+            InitializeComponent();
+        }
+
+        public SecondPage(string userInput)
+        {
+            InitializeComponent();
+            if (userInput == "")
+            {
+                labelInput.Text = "No text enter";
+            }
+            else
+            {
+                labelInput.Text = userInput;
+            }
+        }
+    }
+}
